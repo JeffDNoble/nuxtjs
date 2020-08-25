4c91f53b1b02c7f7ab0fc570b64b61f0a6b4d5f4
JeffDNoble <69968067+JeffDNoble@users.noreply.github.com>
2020-08-25 14:46:04 -0400

    initial commit

---
+++ components/TheNavBar.vue
@@ -0,0 +1,40 @@
+<template>
+
+  <nav class+"nav">
+    <div class="logo">
+      <a href="#" class="logo text-lg">
+
+        jdnoble test
+      </a>
+
+
+
+      <span class"subheader">A jd noble</span>
+
+    
+
+      </div>
+    </nav>
+  </template>
+
+
+  <script>
+  export default {
+    
+  }
+  </script>
+
+  ><style scoped>
+  nav{
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom 70px;
+  font-weight: lighter;
+  letter-spaceing: 0.5px;
+  }
+  
+  </style>
+  
+  
